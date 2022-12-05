6cb1ffd567de97996c4f6e82bda8f8d36e843cf9
MramRashidR <mramrashid98@gmail.com>
2022-12-05 15:24:52 +0300

    adding multiple imgs

--- Platino_Edition/main_app/admin.py
+++ Platino_Edition/main_app/admin.py
@@ -1,9 +1,71 @@
 from django.contrib import admin
-from .models import Car, Realestate, Jet, Yacht
+from .models import Car, Realestate, Jet, Yacht, Multimg
 
 # Register your models here.
-admin.site.register(Car)
-admin.site.register(Realestate)
-admin.site.register(Jet)
-admin.site.register(Yacht)
-# admin.site.register(User)
+# admin.site.register(Car)
+# admin.site.register(Realestate)
+# admin.site.register(Jet)
+# admin.site.register(Yacht)
+# admin.site.register(Multimg)
+
+
+# multiple cars image 
+class CarImageAdmin(admin.StackedInline):
+    model = Multimg
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    inlines = [CarImageAdmin]
+
+    class Meta:
+        model = Car
+
+# multiple realestates image 
+class RealestateImageAdmin(admin.StackedInline):
+    model = Multimg
+
+@admin.register(Realestate)
+class RealestateAdmin(admin.ModelAdmin):
+    inlines = [RealestateImageAdmin]
+
+    class Meta:
+        model = Realestate
+
+# multiple jets image 
+class JetImageAdmin(admin.StackedInline):
+    model = Multimg
+
+@admin.register(Jet)
+class JetAdmin(admin.ModelAdmin):
+    inlines = [JetImageAdmin]
+
+    class Meta:
+        model = Jet
+
+
+# multiple yachts image 
+class YachtImageAdmin(admin.StackedInline):
+    model = Multimg
+
+@admin.register(Yacht)
+class YachtAdmin(admin.ModelAdmin):
+    inlines = [YachtImageAdmin]
+
+    class Meta:
+        model = Yacht
+
+
+
+
+
+
+
+
+
+
+
+
+
+@admin.register(Multimg)
+class MultimgAdmin(admin.ModelAdmin):
+    pass
--- Platino_Edition/main_app/models.py
+++ Platino_Edition/main_app/models.py
@@ -10,37 +10,37 @@ class Car(models.Model):
     year = models.IntegerField(max_length=4)
     description = models.CharField(max_length=120)
     price = models.DecimalField(max_digits=8, decimal_places=3)
-    Images = models.ImageField(upload_to='photos/%y/%m/%d')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Image = models.FileField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
 class Realestate(models.Model):
     tittle = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=8, decimal_places=3)
     space = models.CharField(max_length=60)
-    description = models.CharField(max_length=120)
+    features = models.CharField(max_length=120)
     location = models.CharField(max_length=50)
-    Images = models.ImageField(upload_to='photos/%y/%m/%d')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Image = models.FileField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
 class Jet(models.Model):
     brand = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     year = models.IntegerField(max_length=4)
-    description = models.CharField(max_length=120)
+    features = models.CharField(max_length=120)
     price = models.DecimalField(max_digits=8, decimal_places=3)
     location = models.CharField(max_length=50)
-    Images = models.ImageField(upload_to='photos/%y/%m/%d')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Image = models.FileField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
 class Yacht(models.Model):
     brand = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     year = models.IntegerField(max_length=4)
-    description = models.CharField(max_length=120)
+    features = models.CharField(max_length=120)
     price = models.DecimalField(max_digits=8, decimal_places=3)
     location = models.CharField(max_length=50)
-    Images = models.ImageField(upload_to='photos/%y/%m/%d')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Image = models.FileField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
 # class User(models.Model):
 #     username = models.CharField(max_length=50)
@@ -48,6 +48,14 @@ class Yacht(models.Model):
 #     realestates = models.ForeignKey(Realestate, on_delete=models.CASCADE)
 #     jets = models.ForeignKey(Jet, on_delete=models.CASCADE)
 #     yachts = models.ForeignKey(Yacht, on_delete=models.CASCADE)
+
+class Multimg(models.Model):
+
+    Images = models.FileField(upload_to = 'photos/%y/%m/%d')
+    cars = models.ForeignKey(Car, default=None, on_delete=models.CASCADE, blank=True, null=True)
+    realestates = models.ForeignKey(Realestate, default=None, on_delete=models.CASCADE, blank=True, null=True)
+    jets = models.ForeignKey(Jet, default=None, on_delete=models.CASCADE, blank=True, null=True)
+    yachts = models.ForeignKey(Yacht, default=None, on_delete=models.CASCADE, blank=True, null=True)
 
 
 
