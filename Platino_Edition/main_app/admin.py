8e17269511005b375aca0bd21b6d927bbe0c9ee0
MramRashidR <mramrashid98@gmail.com>
2022-12-05 10:33:06 +0300

    auth modification

--- Platino_Edition/main_app/admin.py
+++ Platino_Edition/main_app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Car, Realestate, Jet, Yacht, User
+from .models import Car, Realestate, Jet, Yacht
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Realestate)
 admin.site.register(Jet)
 admin.site.register(Yacht)
-admin.site.register(User)
+# admin.site.register(User)
